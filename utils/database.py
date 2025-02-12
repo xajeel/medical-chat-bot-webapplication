9d9fe8e33dc4ff72aea6c0792000a847c97b845b
sajeel <sajeel135@gmail.com>
2025-02-12 19:28:58 +0500

    Updates

---
+++ utils/database.py
@@ -0,0 +1,23 @@
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.environ("PINECONE_API_KEY")
+
+def create_database():
+    pc = Pinecone(api_key=api_key)
+
+    # Creating an Index in the Pinecone with name of 'medical'
+
+    index_name = "medical"
+
+    pc.create_index(
+        name=index_name,
+        dimension=768, # Replace with your model dimensions
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
+    )
