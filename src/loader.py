d9b1e0ac4a53ce682f7cf2c2e630ad3ce9a085d5
sajeel <sajeel135@gmail.com>
2025-02-12 19:16:42 +0500

    Updates

--- src/loader.py
+++ src/loader.py
@@ -1,10 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-def load_pdf(path):
+async def load_pdf(path):
     loader = PyPDFLoader(path)
     pages = []
-    for page in loader.alazy_load():
+    async for page in loader.alazy_load():
         pages.append(page)
     return pages
-
-# print('Loaded')
